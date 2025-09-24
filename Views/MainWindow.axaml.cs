4393753440a3fc7c10c7d39d3a158f64b627f0c1
bele255 <bene.ledwig@t-online.de>
2025-09-24 20:35:52 +0200

    rework of timer: -changed from DispatcherTimer to RequestAnimationFrame - moved lerp away from button click, so only one timer is used for all circles - list now working so more than one circle can be added

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,104 +1,114 @@
 using System;
-using System.Diagnostics;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Interactivity;
-using Avalonia.Threading;
 using Avalonia.Media;
 
 namespace ElevatorSimulator.Views;
 
 public partial class MainWindow : Window
 {
-    private DispatcherTimer _timer;
-    private double _startX, _startY, _endX, _endY, _t;
-    private const double Duration = 1.0; // seconds
-    private const double Interval = 0.016; // ~60 FPS
     
-    private Point[] spawn = new Point[]
-    {
-        new Point(200, 100),
-        new Point(200, 200),
-        new Point(200, 300),
-        new Point(200, 400)
-    };
-    
-    private Point[] despawn = new Point[]
-    {
-        new Point(400, 100),
-        new Point(400, 200),
-        new Point(400, 300),
-        new Point(400, 400)
-    };
-    
-    List<Ellipse> circles = new List<Ellipse>();
-    
+    private readonly List<CircleAnimation> animations = new();
+    private readonly Stopwatch stopwatch = new();
     
     public MainWindow()
     {
-        _timer = new DispatcherTimer();
         InitializeComponent();
+        stopwatch.Start();
+        this.RequestAnimationFrame(OnFrame);
     }
 
+    
     private void Button_OnClick(object sender, RoutedEventArgs e)
     {
         Random rnd = new Random();
         int etage = rnd.Next(0, 4);
+        double duration = 1.0;
 
-        var circle = new Ellipse() { Width = 50, Height = 50, Fill = Brushes.MintCream };
+        var spawn = new[]
+        {
+            new Point(200, 100),
+            new Point(200, 200),
+            new Point(200, 300),
+            new Point(200, 400)
+        };
+        
+        var despawn = new[]
+        {
+            new Point(400, 100),
+            new Point(400, 200),
+            new Point(400, 300),
+            new Point(400, 400)
+        };
+        
+        var circle = new Ellipse
+        {
+            Width = 50, 
+            Height = 50, 
+            Fill = Brushes.MintCream
+        };
         
         Canvas.SetLeft(circle, spawn[etage].X);
         Canvas.SetTop(circle, spawn[etage].Y);
         MySymbol.Children.Add(circle);
-        circles.Add(circle);
-        MoveSymbol(circle, despawn[etage]);
+        
+        animations.Add(new CircleAnimation(circle, spawn[etage], despawn[etage], stopwatch.Elapsed.TotalSeconds, duration));
     }
 
-    private void MoveSymbol(Ellipse circle, Point goal)
-    {
-        Point start = new Point (Canvas.GetLeft(circle), Canvas.GetTop(circle));
-
-        StartLerp(circle, start, goal);
-    }
-
-    private void StartLerp(Ellipse circle, Point start, Point goal)
-    {
-        
-        double t = 0;
-        double duration = 1000.0;       // Dauer in Sekunden?
-        double interval = 0.016;        // ~60 FPS
-
-        _t = 0;
-
-        var timer = new DispatcherTimer
-        {
-            Interval = TimeSpan.FromSeconds(Interval)
-        };
-        
-        _timer.Tick += (spawn, e) =>
-        {
-            t += interval / duration;
-            if (t >= 1)
-            {
-                t = 1;
-                timer.Stop();
-
-                MySymbol.Children.Remove(circle);
-                circles.Remove(circle);
-            }
-            
-            double newX = Lerp(start.X, goal.X, t);
-            double newY = Lerp(start.Y, goal.Y, t);
-
-            Canvas.SetLeft(circle, newX);
-            Canvas.SetTop(circle, newY);
-        };
-        
-        _timer.Start();
-    }
-
-    private static double Lerp(double start, double end, double t) => start + (end - start) * t;
     
+    private void OnFrame(TimeSpan frameTime)
+    {
+        double now = stopwatch.Elapsed.TotalSeconds;
+        var finished = new List<CircleAnimation>();
+
+        foreach (var circle in animations)
+        {
+            double t = (now - circle.StartTime) / circle.Duration;
+            if (t >= 1.0)
+            {
+                t = 1.0;
+                finished.Add(circle);
+            }
+
+            double newX = Lerp(circle.Start.X, circle.End.X, t);
+            double newY = Lerp(circle.Start.Y, circle.End.Y, t);
+
+            Canvas.SetLeft(circle.Circle, newX);
+            Canvas.SetTop(circle.Circle, newY);
+        }
+
+        foreach (var circle in finished)
+        {
+            MySymbol.Children.Remove(circle.Circle);
+            animations.Remove(circle);
+        }
+
+        this.RequestAnimationFrame(OnFrame);
+    }
+
+    private static double Lerp(double start, double end, double t)
+        => start + (end - start) * t;
+}
+
+
+public class CircleAnimation
+{
+    public Ellipse Circle { get; }
+    public Point Start { get; }
+    public Point End { get; }
+    public double StartTime { get; }
+    public double Duration { get; }
+
+    public CircleAnimation(Ellipse circle, Point start, Point end, double startTime, double duration)
+    {
+        Circle = circle;
+        Start = start;
+        End = end;
+        StartTime = startTime;
+        Duration = duration;
+    }
 }
